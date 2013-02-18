5df408d85b0df633782a7e479e2120acbf933d25
melkor <melkor@orangepalantir.org>
2013-02-18 20:45:09 +0000

    Again, some crap with passwords and stiff. It would be nice if this could work.

---
+++ src/simplecrypt/SafeCrypt.java
@@ -0,0 +1,205 @@
+package simplecrypt;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.security.AlgorithmParameters;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
+import java.security.spec.KeySpec;
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: melkor
+ * Date: 2/17/13
+ * Time: 9:45 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SafeCrypt {
+    final String encryption_provider = "AES/CBC/PKCS5Padding";
+    char[] password;
+    byte[] salt;
+    ArrayList<String> errors = new ArrayList<String>();
+    SecretKey secret;
+
+    public SafeCrypt(String salt){
+        try {
+            this.salt = salt.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            errors.add("UTF-8 not supported using platform default");
+            this.salt = salt.getBytes();
+            e.printStackTrace();
+        }
+    }
+
+    public void setPassword(String password){
+        this.password = password.toCharArray();
+
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            KeySpec spec = new PBEKeySpec(this.password, salt, 65536, 256);
+            SecretKey tmp = factory.generateSecret(spec);
+            secret = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void encryptAndSave(File outfile, String data){
+
+        /* Encrypt the message. */
+        byte[] bytes_to_write = null;
+        try {
+            Cipher cipher = Cipher.getInstance(encryption_provider);
+            cipher.init(Cipher.ENCRYPT_MODE, secret);
+            AlgorithmParameters params = cipher.getParameters();
+            byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+            byte[] ciphertext = cipher.doFinal(getBytes(data));
+            bytes_to_write= new byte[iv.length+ciphertext.length + 1];
+            bytes_to_write[0] = (byte)iv.length;
+            System.arraycopy(iv,0,bytes_to_write,1,iv.length);
+            System.arraycopy(ciphertext, 0, bytes_to_write, 1+iv.length, ciphertext.length);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidParameterSpecException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            FileOutputStream fos = new FileOutputStream(outfile);
+            fos.write(bytes_to_write);
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    final int chunk_size = 2048;
+    public String decryptFile(File infile){
+        byte[] cipher_text, iv;
+        try {
+
+            FileInputStream fis = new FileInputStream(infile);
+            int reading = 1;
+            int total = 0;
+            ArrayList<byte[]> chunks = new ArrayList<byte[]>();
+            while(reading>0){
+                byte[] chunk = new byte[chunk_size];
+                int read = fis.read(chunk);
+                if(read>0){
+                    total += read;
+                    chunks.add(chunk);
+                } else{
+                    //the file size lined up with the chunk_size.
+                    reading = 0;
+                }
+                if(read<chunk_size){
+                    reading=0;
+                }
+
+            }
+            byte[] l_iv_text = new byte[total];
+            int mapped = 0;
+            for(byte[] chunk: chunks){
+                int remaining = total-mapped;
+                int map_size = (remaining>chunk_size)?chunk_size:remaining;
+                System.arraycopy(chunk, 0, l_iv_text, mapped,map_size );
+                mapped += map_size;
+            }
+
+            int iv_length = l_iv_text[0];
+            iv = new byte[iv_length];
+            System.arraycopy(l_iv_text,1,iv,0,iv_length);
+            int text_length = total-1-iv_length;
+            cipher_text = new byte[text_length];
+            System.arraycopy(l_iv_text,1+iv_length, cipher_text, 0, text_length);
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+
+        byte[] output = null;
+        /* Decrypt the message, given derived key and initialization vector. */
+        try {
+            Cipher cipher = Cipher.getInstance(encryption_provider);
+
+            cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(iv));
+            output = cipher.doFinal(cipher_text);
+
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }  catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+
+        if(output!=null){
+            return getString(output);
+        } else{
+            return null;
+        }
+    }
+
+    /**
+     * Tries to get the bytes of this string assuming UTF-8, if that fails it
+     * uses the default.
+     *
+     * @param s
+     * @return s's bytes.
+     */
+    byte[] getBytes(String s){
+        try{
+            return s.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return s.getBytes();
+        }
+    }
+
+    String getString(byte[] b){
+        try {
+            return new String(b,"UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return new String(b);
+        }
+
+    }
+
+}
+ 
---
+++ src/simplecrypt/SimpleSafeApp.java
@@ -0,0 +1,126 @@
+package simplecrypt;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: melkor
+ * Date: 2/16/13
+ * Time: 2:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimpleSafeApp {
+    JTextPane pane;
+    JFrame frame;
+    File pwd = new File(".");
+    String password;
+    private void buildGUI(){
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        pane = new JTextPane();
+        frame.add(pane, BorderLayout.CENTER);
+
+        JMenuBar bar = new JMenuBar();
+        JMenu file = new JMenu("file");
+        bar.add(file);
+
+        JMenuItem load = new JMenuItem("load");
+        file.add(load);
+
+        load.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loadText();
+            }
+        });
+
+        JMenuItem save = new JMenuItem("save");
+        file.add(save);
+
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                saveText();
+            }
+        });
+
+        JMenuItem setPassword = new JMenuItem("enter password");
+        file.add(setPassword);
+        setPassword.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getPassword();
+            }
+        });
+
+        frame.setJMenuBar(bar);
+        frame.setSize(new Dimension(600, 600));
+        frame.setVisible(true);
+
+    }
+
+    public void getPassword(){
+        password = JOptionPane.showInputDialog("Enter Password");
+    }
+
+    public void loadText(){
+        JFileChooser chooser = new JFileChooser(pwd);
+        chooser.showDialog(frame,"Open Encrypted File");
+        File input = chooser.getSelectedFile();
+        if(input!=null){
+            pwd = chooser.getCurrentDirectory();
+        } else{
+            //cancelled
+            return;
+        }
+
+        if(password==null){
+            getPassword();
+        }
+
+        SafeCrypt crypt = new SafeCrypt("simple safe");
+        crypt.setPassword(password);
+        String text = crypt.decryptFile(input);
+        pane.setText(text);
+
+    }
+
+    public void saveText(){
+        JFileChooser chooser = new JFileChooser("Save Encrypted File");
+        chooser.setCurrentDirectory(pwd);
+        chooser.showSaveDialog(frame);
+        File output = chooser.getSelectedFile();
+        if(output!=null){
+            pwd = chooser.getCurrentDirectory();
+        } else{
+            return;
+        }
+        if(password==null){
+            getPassword();
+        }
+
+        SafeCrypt crypt = new SafeCrypt("simple safe");
+        crypt.setPassword(password);
+
+        String text = pane.getText();
+        crypt.encryptAndSave(output, text);
+
+    }
+
+    public static void main(String[] args){
+        final SimpleSafeApp ssa = new SimpleSafeApp();
+        EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                ssa.buildGUI();
+            }
+        });
+    }
+}
+ 
